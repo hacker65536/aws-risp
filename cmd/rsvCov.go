d128d87f74fe1171f3c0b683f9b839ad4adcff44
hacker65536 <s.hacker65536@gmail.com>
2024-12-13 04:25:14 +0900

    fix cmd usage

--- cmd/rsvCov.go
+++ cmd/rsvCov.go
@@ -17,12 +17,7 @@ var start, end string
 var rsvCovCmd = &cobra.Command{
 	Use:   "rsvCov",
 	Short: "Reservation Coverage",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		//fmt.Println("rsvCov called")
 		if len(args) == 0 {
