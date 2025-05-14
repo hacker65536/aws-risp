161dce204698d21bf7a264a4e17f7788eaaa473b
hacker65536 <s.hacker65536@gmail.com>
2025-05-14 14:58:21 +0900

    fix

--- cmd/version.go
+++ cmd/version.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"runtime"
 	"runtime/debug"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -30,43 +31,79 @@ and Go runtime information.`,
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
+
+	// バージョンコマンドを登録し、バージョン情報を設定
+	if version != "" {
+		// 先頭の "v" が重複しないように調整
+		if strings.HasPrefix(version, "v") {
+			rootCmd.Version = version
+		} else {
+			rootCmd.Version = "v" + version
+		}
+	}
 }
 
 // displayVersion は詳細なバージョン情報を表示する
 func displayVersion() {
-	// バージョン情報が設定されていない場合は、ビルド情報から取得を試みる
-	if version == "" && commit == "" {
-		displayBuildInfo()
-	} else {
-		displayStaticInfo()
-	}
+	// バージョン情報の取得
+	versionInfo := getVersionInfo()
 
+	// バージョン情報の表示
+	fmt.Println("AWS Reserved Instance and Savings Plan CLI")
+	fmt.Println("------------------------------------------")
+	fmt.Printf("Version:    %s\n", versionInfo.version)
+	fmt.Printf("Commit:     %s\n", versionInfo.commit)
+	fmt.Printf("Built:      %s\n", versionInfo.date)
 	fmt.Printf("Go version: %s\n", runtime.Version())
 	fmt.Printf("OS/Arch:    %s/%s\n", runtime.GOOS, runtime.GOARCH)
 }
 
-func displayBuildInfo() {
-	info, ok := debug.ReadBuildInfo()
-	if !ok {
-		fmt.Println("Build information not available.")
-		return
+// バージョン情報を保持する構造体
+type versionInformation struct {
+	version string
+	commit  string
+	date    string
+}
+
+// getVersionInfo はビルド情報とコンパイル時情報を組み合わせてバージョン情報を返す
+func getVersionInfo() versionInformation {
+	// 基本情報を設定
+	info := versionInformation{
+		version: version,
+		commit:  commit,
+		date:    date,
 	}
 
-	fmt.Printf("Version:    %s\n", info.Main.Version)
+	// コンパイル時情報が設定されていない場合はビルド情報から取得
+	needBuildInfo := info.version == "" || info.commit == ""
 
-	commitHash := "unknown"
-	for _, setting := range info.Settings {
-		if setting.Key == "vcs.revision" {
-			commitHash = setting.Value
-			break
+	if needBuildInfo {
+		buildInfo, ok := debug.ReadBuildInfo()
+		if ok {
+			// バージョンが設定されていない場合
+			if info.version == "" && buildInfo.Main.Version != "" {
+				info.version = buildInfo.Main.Version
+			}
+
+			// コミットハッシュが設定されていない場合
+			if info.commit == "" {
+				for _, setting := range buildInfo.Settings {
+					if setting.Key == "vcs.revision" {
+						info.commit = setting.Value
+						break
+					}
+				}
+			}
 		}
 	}
 
-	fmt.Printf("Commit:     %s\n", commitHash)
-}
+	// 空文字列の場合はunknownを設定
+	if info.version == "" {
+		info.version = "unknown"
+	}
+	if info.commit == "" {
+		info.commit = "unknown"
+	}
 
-func displayStaticInfo() {
-	fmt.Printf("Version:    %s\n", version)
-	fmt.Printf("Commit:     %s\n", commit)
-	fmt.Printf("Built:      %s\n", date)
+	return info
 }
